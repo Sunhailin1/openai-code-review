4811b9bc891a3c9ca572e878adbf03da6b3319ab
BestHl <497724416@qq.com>
2025-04-11 10:44:09 +0800

    feat: 配置ChatGLM代码审查

--- openai-code-review-sdk/src/main/java/cn/shl/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/cn/shl/sdk/OpenAiCodeReview.java
@@ -36,7 +36,7 @@ public class OpenAiCodeReview {
     }
 
     private static String codeReview(String diffCode) throws Exception{
-        String apiKeySecret = "c78fbacd3e10118ad5649d7a54a3a163.UunYDBxpzeClvSKZ";
+        String apiKeySecret = "ffd570bd2460459ba2b1c4a9cfb80bc8.Go6m9ylXDetG30BN";
         String token = BearerTokenUtils.getToken(apiKeySecret);
 
         URL url = new URL("https://open.bigmodel.cn/api/paas/v4/chat/completions");
